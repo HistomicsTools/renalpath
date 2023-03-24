71d06d8f2cafc13d816030ea1f15fb1f08c5c5de
David Manthey <david.manthey@kitware.com>
2023-03-24 12:12:41 -0400

    More data in popover.

--- renalpath/web_client/views/AnnotationPopover.js
+++ renalpath/web_client/views/AnnotationPopover.js
@@ -12,9 +12,10 @@ wrap(AnnotationPopover, '_elementAdditionalValues', function (_elementAdditional
         return element._additionalValues;
     }
     element._additionalValues = null;
-    let topkey = 'Main_Cell_Types';
     let ctypes;
-    if ((element.get('user') || {})[topkey]) {
+    const topkey = 'Main_Cell_Types';
+    const user = element.get('user') || {};
+    if (user[topkey]) {
         ctypes = element.get('user')[topkey];
     } else {
         const attrib = annotation.get('annotation').attributes;
@@ -24,11 +25,17 @@ wrap(AnnotationPopover, '_elementAdditionalValues', function (_elementAdditional
         ctypes = attrib[topkey][element.id];
     }
     if (ctypes) {
-        let values = Object.keys(ctypes).filter((key) => ctypes[key]).sort((a, b) => ctypes[b] - ctypes[a]).map((key) => `${key}: ${ctypes[key].toFixed(3)}`);
+        let values = Object.keys(ctypes).filter((key) => ctypes[key] && ctypes[key] >= 0.0005).sort((a, b) => ctypes[b] - ctypes[a]).map((key) => `${key}: ${ctypes[key].toFixed(3)}`);
         if (values.length) {
             results += '<div>' + values.join('</div><div>') + '</div>';
         }
     }
+    Object.keys(user).filter((key) => user[key].substr || (user[key].toFixed && Math.abs(user[key]) >= 0.0005)).sort().forEach((key) => {
+        const val = user[key].toFixed ? user[key].toFixed(3) : ('' + user[key]);
+        if (val) {
+            results += `<div>${key}: ${val}</div>`;
+        }
+    });
     element._additionalValues = results;
     return results;
 });
