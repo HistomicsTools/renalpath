000fa4e7dc960bcb4a56f7a6cab3d315f2d8e854
David Manthey <david.manthey@kitware.com>
2024-07-12 16:51:11 -0400

    Guard against miscellaneous exceptions

--- renalpath/web_client/views/MetadataPlot.js
+++ renalpath/web_client/views/MetadataPlot.js
@@ -8,56 +8,60 @@ import MetadataPlot from '@girder/histomicsui/panels/MetadataPlot';
 
 wrap(MetadataPlot, 'onHover', function (onHover, event) {
     let results = onHover.apply(this, _.rest(arguments, 1));
-    if (event && event.points && event.points.length >= 1 && event.points[0].pointIndex !== undefined) {
-        const idx = event.points[0].pointIndex;
-        let image;
-        if (this.lastPlotData.data[idx]._roots) {
-            const roots = this.lastPlotData.data[idx]._roots;
-            Object.entries(roots).forEach(([key, value]) => {
-                if (!image && value.image_id && value.Min_x_coord !== undefined && value.Max_x_coord !== undefined && value.Min_y_coord !== undefined && value.Min_y_coord !== undefined) {
-                    image = {
-                        id: value.image_id,
-                        left: value.Min_x_coord,
-                        top: value.Min_y_coord,
-                        right: value.Max_x_coord,
-                        bottom: value.Max_y_coord
-                    };
+    try {
+        if (event && event.points && event.points.length >= 1 && event.points[0].pointIndex !== undefined) {
+            const idx = event.points[0].pointIndex;
+            let image;
+            if (this.lastPlotData.data[idx]._roots) {
+                const roots = this.lastPlotData.data[idx]._roots;
+                Object.entries(roots).forEach(([key, value]) => {
+                    if (!image && value.image_id && value.Min_x_coord !== undefined && value.Max_x_coord !== undefined && value.Min_y_coord !== undefined && value.Min_y_coord !== undefined) {
+                        image = {
+                            id: value.image_id,
+                            left: value.Min_x_coord,
+                            top: value.Min_y_coord,
+                            right: value.Max_x_coord,
+                            bottom: value.Max_y_coord
+                        };
+                    }
+                });
+            } else {
+                image = this.lastPlotData.data[idx].image;
+            }
+            if (image && (!this.lastPlotData._images || !this.lastPlotData._images[image.id])) {
+                this.lastPlotData._images = this.lastPlotData._images || {};
+                if (!this.lastPlotData._images[image.id]) {
+                    this.lastPlotData._images[image.id] = restRequest({
+                        url: 'item', data: {folderId: this.parentFolderId, text: `"${image.id}"`}, error: null});
                 }
-            });
-        } else {
-            image = this.lastPlotData.data[idx].image;
-        }
-        if (image && (!this.lastPlotData._images || !this.lastPlotData._images[image.id])) {
-            this.lastPlotData._images = this.lastPlotData._images || {};
-            if (!this.lastPlotData._images[image.id]) {
-                this.lastPlotData._images[image.id] = restRequest({
-                    url: 'item', data: {folderId: this.parentFolderId, text: `"${image.id}"`}, error: null});
+            }
+            if (image) {
+                this._lastThumbnailRegionPromise = this.lastPlotData._images[image.id];
+                this._lastThumbnailRegionPromise.done((items) => {
+                    if (this.lastPlotData._images[image.id] !== this._lastThumbnailRegionPromise || !items || items.length !== 1 || $('svg g.hoverlayer').length === 0) {
+                        return;
+                    }
+                    const regionUrl = `api/v1/item/${items[0]._id}/tiles/region?width=100&height=100&left=${image.left}&top=${image.top}&right=${image.right}&bottom=${image.bottom}`;
+                    let x = parseFloat($('svg g.hoverlayer g.hovertext text[x]').attr('x'));
+                    let y = parseFloat($('svg g.hoverlayer g.hovertext text[y]').attr('y'));
+                    x = x === 0 ? -50 : x < 0 ? x - 100 : x;
+                    let d = $('svg g.hoverlayer g.hovertext path[d]').attr('d');
+                    if (d && (d.split('L6,').length >= 2 || d.split('L-6,').length >= 2)) {
+                        let y2 = parseFloat(d.split('v')[2]);
+                        y += Math.abs(y2) - 100 - 13;
+                    } else if (d && d.split('v').length === 2) {
+                        let y2 = parseFloat(d.split('v')[1]);
+                        y += Math.abs(y2) - 100 - 13;
+                    } else {
+                        y = -3;
+                    }
+                    $('svg g.hoverlayer g.hovertext image.hoverthumbnail').remove();
+                    $('svg g.hoverlayer g.hovertext').html($('svg g.hoverlayer g.hovertext').html() + `<image class="hoverthumbnail" href="${regionUrl}" x="${x}px" y="${y}px" width="100px" height="100px"/>`);
+                });
             }
         }
-        if (image) {
-            this._lastThumbnailRegionPromise = this.lastPlotData._images[image.id];
-            this._lastThumbnailRegionPromise.done((items) => {
-                if (this.lastPlotData._images[image.id] !== this._lastThumbnailRegionPromise || !items || items.length !== 1 || $('svg g.hoverlayer').length === 0) {
-                    return;
-                }
-                const regionUrl = `api/v1/item/${items[0]._id}/tiles/region?width=100&height=100&left=${image.left}&top=${image.top}&right=${image.right}&bottom=${image.bottom}`;
-                let x = parseFloat($('svg g.hoverlayer g.hovertext text[x]').attr('x'));
-                let y = parseFloat($('svg g.hoverlayer g.hovertext text[y]').attr('y'));
-                x = x === 0 ? -50 : x < 0 ? x - 100 : x;
-                let d = $('svg g.hoverlayer g.hovertext path[d]').attr('d');
-                if (d && (d.split('L6,').length >= 2 || d.split('L-6,').length >= 2)) {
-                    let y2 = parseFloat(d.split('v')[2]);
-                    y += Math.abs(y2) - 100 - 13;
-                } else if (d && d.split('v').length === 2) {
-                    let y2 = parseFloat(d.split('v')[1]);
-                    y += Math.abs(y2) - 100 - 13;
-                } else {
-                    y = -3;
-                }
-                $('svg g.hoverlayer g.hovertext image.hoverthumbnail').remove();
-                $('svg g.hoverlayer g.hovertext').html($('svg g.hoverlayer g.hovertext').html() + `<image class="hoverthumbnail" href="${regionUrl}" x="${x}px" y="${y}px" width="100px" height="100px"/>`);
-            });
-        }
+    } catch (err) {
+        console.log('Failed in onHover', err);
     }
     return results;
 });
