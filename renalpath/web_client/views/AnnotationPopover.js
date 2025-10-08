5ab0172351e667c558672a22c672fbb7781e6543
David Manthey <david.manthey@kitware.com>
2025-10-08 16:32:17 -0400

    Handle more data in popover

--- renalpath/web_client/views/AnnotationPopover.js
+++ renalpath/web_client/views/AnnotationPopover.js
@@ -29,18 +29,24 @@ wrap(AnnotationPopover, '_elementAdditionalValues', function (_elementAdditional
         }
         ctypes = attrib[topkey][element.id];
     }
+    let values = [];
     if (ctypes) {
-        let values = Object.keys(ctypes).filter((key) => ctypes[key] && ctypes[key] >= 0.0005).sort((a, b) => ctypes[b] - ctypes[a]).map((key) => `${key}: ${ctypes[key].toFixed(Number.isInteger(ctypes[key]) ? 0 : 3)}`);
-        if (values.length) {
-            results += '<div>' + values.join('</div><div>') + '</div>';
-        }
+        values = Object.keys(ctypes).filter((key) => ctypes[key] && ctypes[key] >= 0.0005).sort((a, b) => ctypes[b] - ctypes[a]).map((key) => `${key}: ${ctypes[key].toFixed(Number.isInteger(ctypes[key]) ? 0 : 3)}`);
     }
     Object.keys(user).filter((key) => user[key].substr || (user[key].toFixed && Math.abs(user[key]) >= 0.0005)).sort().forEach((key) => {
         const val = user[key].toFixed ? user[key].toFixed(Number.isInteger(user[key]) ? 0 : 3) : ('' + user[key]);
         if (val) {
-            results += `<div>${key}: ${val}</div>`;
+            values.push(`${key}: ${val}`);
         }
     });
+    if (values.length) {
+        if (values.length <= 30) {
+            results += '<div>' + values.join('</div><div>') + '</div>';
+        } else {
+            let subdiv = '<div style="break-inside: avoid; overflow: hidden">';
+            results += '<div style="column-count: 2; column-gap=10px;">' + subdiv + values.join('</div>' + subdiv) + '</div></div>';
+        }
+    }
     element._additionalValues = results;
     return results;
 });
